b6be0523772e6c452624a06ff80ba8ce261c78db
HenriqueBaesa <62488513+HenriqueBaesa@users.noreply.github.com>
2022-11-27 21:37:49 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 
+// mudar imagem com jQuery
+$(document).ready(function() {
+
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+});
 
 
 // corrossel - henrique
